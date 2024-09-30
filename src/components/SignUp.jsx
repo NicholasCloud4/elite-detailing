3d2f8356f43e40a64e7037fd2442794cc4c7f8bc
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-09-30 10:24:29 -0400

    updated text

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -6,12 +6,12 @@ const SignUp = () => {
         <div className={styles.container}>
             <div className={styles.text} >
                 <p id='signup' className={styles.title}>STAY IN TOUCH</p>
-                <p>Sign up for our mailing list to receive new product alerts, special offers, and coupon codes.</p>
+                <p>Sign up to our mailing list to receive new product alerts, special offers, and coupon codes.</p>
             </div>
             <form className={styles.form} action="">
                 <input className={styles.input} type="text" placeholder='Name' />
                 <input className={styles.input} type="text" placeholder='Email' />
-                <button className={styles.button}>Subscribe</button>
+                <button className={styles.button}>Sign Up</button>
             </form>
         </div>
     )
